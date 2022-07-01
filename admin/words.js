3c1c601311ddda20091d7392e0388a6b97000eee
andy <andy@debian-gnu-linux-10.localdomain>
2022-07-01 21:26:32 +0200

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"reolink adapter settings": {
+		"en": "Adapter settings for reolink",
+		"de": "Adaptereinstellungen für reolink",
+		"ru": "Настройки адаптера для reolink",
+		"pt": "Configurações do adaptador para reolink",
+		"nl": "Adapterinstellingen voor reolink",
+		"fr": "Paramètres d'adaptateur pour reolink",
+		"it": "Impostazioni dell'adattatore per reolink",
+		"es": "Ajustes del adaptador para reolink",
+		"pl": "Ustawienia adaptera dla reolink",
+		"zh-cn": "reolink的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "Option 1",
+		"ru": "Опция 1",
+		"pt": "Opção 1",
+		"nl": "Optie 1",
+		"fr": "Option 1",
+		"it": "opzione 1",
+		"es": "Opción 1",
+		"pl": "opcja 1",
+		"zh-cn": "选项1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "Option 2",
+		"ru": "вариант 2",
+		"pt": "opção 2",
+		"nl": "Optie 2",
+		"fr": "Option 2",
+		"it": "opzione 2",
+		"es": "opcion 2",
+		"pl": "Opcja 2",
+		"zh-cn": "选项2"
+	}
+};
---
+++ main.js
@@ -0,0 +1,239 @@
+"use strict";
+
+/*
+ * Created with @iobroker/create-adapter v2.1.1
+ */
+
+const utils = require("@iobroker/adapter-core");
+const axios = require("axios").default;
+const https = require("https");
+
+
+class TestProject extends utils.Adapter {
+
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	constructor(options) {
+		super({
+			...options,
+			name: "reolink",
+		});
+
+		this.apiConnected = false;
+		this.reolinkApiClient = null;
+		this.cameraModel = null;
+		this.refreshStateTimeout = null;
+
+		this.on("ready", this.onReady.bind(this));
+		this.on("stateChange", this.onStateChange.bind(this));
+		// this.on("objectChange", this.onObjectChange.bind(this));
+		// this.on("message", this.onMessage.bind(this));
+		this.on("unload", this.onUnload.bind(this));
+
+	}
+
+	async onReady() {
+
+		this.log.info("Reolink adapter has started");
+		if (!this.config.cameraIp){
+			this.log.error("Camera Ip not set - please check instance!");
+			return;
+		}
+		if (!this.config.cameraType){
+			this.log.error("Camera type not set - please check instance!");
+			return;
+		}
+		if(!this.config.cameraUser || !this.config.cameraPassword){
+			this.log.error("Username and/or password not set properly - please check instance!");
+			return;
+		}
+
+		this.reolinkApiClient = axios.create({
+			baseURL: `https://${this.config.cameraIp}`,
+			timeout: 4000,
+			responseType: "json",
+			responseEncoding: "utf8",
+			httpsAgent: new https.Agent({
+				rejectUnauthorized: false,
+			}),
+		});
+		this.log.info(`Current IP: ${this.config.cameraIp}`);
+		await this.setStateAsync("Network.Ip",{val: this.config.cameraIp, ack: true});
+		await this.setStateAsync("Network.Channel",{val: this.config.cameraChannel, ack: true});
+
+		this.log.info(`Current Devicetype: ${this.config.cameraType}`);
+		
+		if(this.config.cameraType == "rlc510A"){
+			this.getDevinfo();
+			this.getLocalLink();
+			this.refreshState("onReady");
+		}else if(this.config.cameraType == "others"){
+
+			this.log.info("camera type set to others...API functions not testet!");
+			this.getDevinfo();
+			this.getLocalLink();
+			this.refreshState("onReady");
+		}else {
+			this.log.error("Cameratyp undefined...");
+		}
+
+	}
+	//function for getting motion detection
+	async getMdState(){
+		try {
+			const MdInfoValues = await this.reolinkApiClient.get(`/api.cgi?cmd=GetMdState&channel=${this.config.cameraChannel}&user=${this.config.cameraUser}&password=${this.config.cameraPassword}`);
+
+			this.log.debug(`camMdStateInfo ${JSON.stringify(MdInfoValues.status)}: ${JSON.stringify(MdInfoValues.data)}`);
+
+			if(MdInfoValues.status === 200){
+				this.apiConnected = true;
+				await this.setStateAsync("Network.Connected", {val: this.apiConnected, ack: true});
+
+				var MdValues = MdInfoValues.data[0];
+
+				this.log.info(MdValues.value.state);
+				await this.setStateAsync("States.Md", {val: MdValues.value.state, ack: true});
+
+			}
+		} catch (error) {
+			this.apiConnected = false;
+			await this.setStateAsync("Network.Connected", {val: this.apiConnected, ack: true});
+			this.log.error(error);
+		}
+	}
+	//function for getting general information of camera device
+	async getDevinfo(){
+		try {
+			const DevInfoValues = await this.reolinkApiClient.get(`/api.cgi?cmd=GetDevInfo&channel=0&user=${this.config.cameraUser}&password=${this.config.cameraPassword}`);
+			this.log.debug(`camMdStateInfo ${JSON.stringify(DevInfoValues.status)}: ${JSON.stringify(DevInfoValues.data)}`);
+
+			if(DevInfoValues.status === 200){
+				this.apiConnected = true;
+				await this.setStateAsync("Network.Connected", {val: this.apiConnected, ack: true});
+				var DevValues = DevInfoValues.data[0];
+
+				await this.setStateAsync("Device.BuildDay", {val: DevValues.value.DevInfo.buildDay, ack: true});
+				await this.setStateAsync("Device.CfgVer", {val: DevValues.value.DevInfo.cfgVer, ack: true});
+				await this.setStateAsync("Device.Detail", {val: DevValues.value.DevInfo.detail, ack: true});
+				await this.setStateAsync("Device.DiskNum", {val: DevValues.value.DevInfo.diskNum, ack: true});
+				await this.setStateAsync("Device.FirmVer", {val: DevValues.value.DevInfo.firmVer, ack: true});
+				await this.setStateAsync("Device.Model", {val: this.cameraModel, ack: true});
+				await this.setStateAsync("Device.Name", {val: DevValues.value.DevInfo.name, ack: true});
+				await this.setStateAsync("Device.Serial", {val: DevValues.value.DevInfo.serial, ack: true});
+				await this.setStateAsync("Device.Wifi", {val: DevValues.value.DevInfo.wifi, ack: true});
+			}
+
+		} catch (error) {
+			this.apiConnected = false;
+			await this.setStateAsync("Network.Connected", {val: this.apiConnected, ack: true});
+
+			this.log.error(error);
+		}
+	}
+
+	async getLocalLink(){
+		try {
+			const LinkInfoValues = await this.reolinkApiClient.get(`/api.cgi?cmd=GetLocalLink&channel=0&user=${this.config.cameraUser}&password=${this.config.cameraPassword}`);
+			this.log.debug(`LinkInfoValues ${JSON.stringify(LinkInfoValues.status)}: ${JSON.stringify(LinkInfoValues.data)}`);
+
+			if(LinkInfoValues.status === 200){
+				this.apiConnected = true;
+				await this.setStateAsync("Network.Connected", {val: this.apiConnected, ack: true});
+				var LinkValues = LinkInfoValues.data[0];
+
+				await this.setStateAsync("Network.ActiveLink", {val: LinkValues.value.LocalLink.activeLink, ack: true});
+				await this.setStateAsync("Network.Mac", {val: LinkValues.value.LocalLink.mac, ack: true});
+				await this.setStateAsync("Network.Dns", {val: LinkValues.value.LocalLink.dns.dns1, ack: true});
+				await this.setStateAsync("Network.Gateway", {val: LinkValues.value.LocalLink.static.gateway, ack: true});
+				await this.setStateAsync("Network.Mask", {val: LinkValues.value.LocalLink.static.mask, ack: true});
+				await this.setStateAsync("Network.NetworkType", {val: LinkValues.value.LocalLink.type, ack: true});
+			}
+		} catch (error) {
+			this.apiConnected = false;
+			await this.setStateAsync("Network.Connected", {val: this.apiConnected, ack: true});
+
+			this.log.error(error);
+		}
+	}
+
+	async refreshState(source){
+		this.log.debug(`refreshState': started from "${source}"`);
+
+		this.getMdState();
+
+		//Delete Timer
+		if(this.refreshStateTimeout){
+			this.log.debug(`refreshStateTimeout: CLEARED by ${source}`);
+			this.clearTimeout(this.refreshStateTimeout);
+		}
+
+
+		//Cretae new Timer (to re-run actions)
+		if(!this.apiConnected){
+			const notConnectedTimeout = 10;
+			this.refreshStateTimeout = this.setTimeout(() => {
+				this.refreshStateTimeout = null;
+				this.refreshState("timeout (API not connected)");
+			}, notConnectedTimeout * 1000);
+			this.log.debug(`refreshStateTimeout: re-created refresh timeout (API not connected): id ${this.refreshStateTimeout}- secounds: ${notConnectedTimeout}`);
+
+		} else {
+			this.refreshStateTimeout = this.setTimeout(() => {
+				this.refreshStateTimeout = null;
+				this.refreshState("timeout(default");
+			}, this.config.apiRefreshInterval * 1000);
+			this.log.debug(`refreshStateTimeout: re-created refresh timeout (default): id ${this.refreshStateTimeout}- secounds: ${this.config.apiRefreshInterval}`);
+
+		}
+
+
+	}
+	/**
+	 * Is called when adapter shuts down - callback has to be called under any circumstances!
+	 * @param {() => void} callback
+	 */
+	onUnload(callback) {
+		try {
+			// Here you must clear all timeouts or intervals that may still be active
+			// clearTimeout(timeout1);
+			// clearTimeout(timeout2);
+			// ...
+			// clearInterval(interval1);
+			if(this.refreshStateTimeout){
+				this.log.debug("refreshStateTimeout: UNLOAD");
+				this.clearTimeout(this.refreshStateTimeout);
+			}
+
+			callback();
+		} catch (e) {
+			callback();
+		}
+	}
+
+	/**
+	 * Is called if a subscribed state changes
+	 * @param {string} id
+	 * @param {ioBroker.State | null | undefined} state
+	 */
+	onStateChange(id, state) {
+		if (state) {
+			// The state was changed
+			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
+		} else {
+			// The state was deleted
+			this.log.info(`state ${id} deleted`);
+		}
+	}
+}
+
+if (require.main !== module) {
+	// Export the constructor in compact mode
+	/**
+	 * @param {Partial<utils.AdapterOptions>} [options={}]
+	 */
+	module.exports = (options) => new TestProject(options);
+} else {
+	// otherwise start the instance directly
+	new TestProject();
+}
